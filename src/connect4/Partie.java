d6430deac8d643b6a87c4832ee6bbb5f29b6368f
Elias Leinenweber <elias.leinenweber@etu.unistra.fr>
2020-12-03 13:42:20 +0100

    Premier push projet IntelliJ

---
+++ src/connect4/Partie.java
@@ -0,0 +1,15 @@
+package connect4;
+
+public class Partie {
+    private int manche;
+    private int nbManches;
+
+    public Partie(int nbManches) {
+        this.nbManches = nbManches;
+        manche = 0;
+    }
+
+    public void initialiser() {
+
+    }
+}
