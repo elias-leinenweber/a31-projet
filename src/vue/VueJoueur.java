b29727ce16b13fe9773b1807997e93b87296c7c4
Emma Sattler <e.sattler@etu.unistra.fr>
2020-12-08 12:26:36 +0100

    Fenetre principale

--- src/vue/VueJoueur.java
+++ src/vue/VueJoueur.java
@@ -5,6 +5,7 @@ import connect4.Partie;
 import observateur.Observateur;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class VueJoueur extends JFrame implements Observateur {
@@ -16,9 +17,30 @@ public class VueJoueur extends JFrame implements Observateur {
         Partie partie=new Partie("Emile","Jules",4 );
         setVisible(true);
         setSize(400,400);
-        setLocationRelativeTo(null);
-    }
 
+        //Panel haut avec noms Joueur + couleur
+        JPanel jpTop = new JPanel(new GridLayout(1, 4));
+        jpTop.add(new JPanel()).setBackground(Color.YELLOW);
+        jpTop.add(new JLabel(partie.getJoueurs()[0].toString()));
+        jpTop.add(new JLabel(partie.getJoueurs()[1].toString()));
+        jpTop.add(new JPanel()).setBackground(Color.RED);
+        jpTop.getComponent(0).setSize(20,20);
+
+        this.add(jpTop, BorderLayout.NORTH);
+        jpTop.setSize(400,20);
+        jpTop.setOpaque(true);
+        setLocationRelativeTo(null);
+
+        /*ImageIcon board = new ImageIcon( getClass().getClassLoader().getResource("Board.png") ); // dans le dossier bin/
+        add(new JLabel(board));
+        board.setBounds(20, 20, imageBoard.getIconWidth(), imageBoard.getIconHeight());
+        //this.add(new GridLayout(partie.getGrid().getLargeur(), partie.getGrid.getHauteur()));
+        int i =0;
+        //while (i<
+
+
+    }
+*/
     public static ArrayList<VueJoueur> getVues(){return vues;}
 
     @Override
