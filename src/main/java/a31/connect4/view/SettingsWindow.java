7222ad6883308c0a6c49e0dce940c6a957c17bc5
Emma Sattler <e.sattler@etu.unistra.fr>
2021-01-07 15:51:36 +0100

    Ajout btn dans menu option

--- src/main/java/a31/connect4/view/SettingsWindow.java
+++ src/main/java/a31/connect4/view/SettingsWindow.java
@@ -1,17 +1,30 @@
 package a31.connect4.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SettingsWindow extends JFrame {
     public SettingsWindow() {
         super("Options");
-        JButton jbConnect5=new JButton("Connect5");
-        jbConnect5.setSize(30,40);
-        add(jbConnect5);
+        JButton jb5Row=new JButton("5-in-a-row");
+        JButton jbHello=new JButton("Hello");
 
+
+        JPanel pnlButtons = new JPanel();
+        pnlButtons.setLayout(new GridLayout(2, 1, 10, 0));
+        pnlButtons.setBorder(BorderFactory.createEmptyBorder(10, 5, 20, 5));
+        pnlButtons.add(jb5Row);
+        pnlButtons.add(jbHello);
+
+        add(pnlButtons);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
+        setSize(200,150);
         setVisible(true);
+
+        //jbConnect5.addActionListener(e->{});
+        jbHello.addActionListener(e->{});
+        }
     }
-}
+
